edd0330db7e39b69884d1053b9fabe7df43a3fed
Ali <ali@afalahi.com>
2017-10-04 14:32:21 -0400

    Create Service-Account-Report.sql

---
+++ Service-Account-Report.sql
@@ -0,0 +1,27 @@
+SELECT 
+	cd.AccountDomain AS 'Domain',
+	cd.AccountName AS 'Account',
+	c.ComputerName AS 'Host Name',
+	cd.DependencyName AS 'Dependency Name',
+	sdt.SecretDependencyTypeName AS 'Dependency Type',
+	SDTM.SecretDependencyTemplateName AS 'Dependency Template Name',
+	CONVERT(VARCHAR(20),c.LastPolledDate,107) AS 'Last Scanned'
+FROM 
+		tbComputer c
+	JOIN 	tbComputerDependency cd 
+
+	ON 
+		cd.ComputerID = c.ComputerId
+
+	JOIN 	tbSecretDependencyType sdt
+
+	ON 
+		sdt.SecretDependencyTypeId = cd.SecretDependencyTypeID
+		
+	JOIN tbSecretDependencyTemplate sdtm
+	
+	ON	
+		cd.ScanItemTemplateId = sdtm.ScanItemTemplateId
+	AND 
+		cd.SecretDependencyTypeID = sdtm.SecretDependencyTypeId
+	ORDER BY cd.AccountName asc
